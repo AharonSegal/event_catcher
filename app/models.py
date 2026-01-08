91091ddf0f025149d4b75f53e03d7a338e673860
AharonSegal <asegal1996@gmail.com>
2026-01-08 17:25:18 +0200

    init

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
-from pydantic import BaseModel
-from datetime import datetime
-from typing import Optional
-
-class Event(BaseModel):
-    event_type: str
-    source: str
-    timestamp: Optional[datetime] = None
-    payload: dict
+"""
+EVENT example:
+{
+"id":"14074595",
+"case_number":"JK104437",
+"date":"2025-12-31T00:00:00.000",
+"block":"065XX S FAIRFIELD AVE",
+"iucr":"1310",
+"primary_type":"CRIMINAL DAMAGE",
+"description":"TO PROPERTY",
+"location_description":"RESIDENCE",
+"arrest":false,
+"domestic":false,
+"beat":"0831",
+"district":"008",
+"ward":"16",
+"community_area":"66",
+"fbi_code":"14",
+"year":"2025",
+"updated_on":"2026-01-07T15:44:10.000"
+}
+"""
